2da06fd5f365509d8335e187b1ff6017e798525e
rojarbel <rojarbel@gmail.com>
2025-06-09 14:12:53 +0300

    revize

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
 import { Slot } from 'expo-router';
-import { AuthProvider } from './src/context/AuthContext';
+
 
 export default function App() {
-  return (
-    <AuthProvider>
-      <Slot />
-    </AuthProvider>
-  );
+ return <Slot />;
 }
